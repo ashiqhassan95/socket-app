f9c60af988ba8d46c2ba23afe06c3d1e904ec23f
ashiqhassan95 <ashiqhassan95@gmail.com>
2020-07-05 12:09:37 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const publicDir = {
+    root: __dirname + '/public',
+};
+
+// Routes
+app.get('/', (request, response) => {
+    response.sendFile('index.html', publicDir);
+});
+
+// Listen on every connection
+// Whenever someone connects this gets executed
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    // Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function() {
+        console.log('User disconnected');
+    });
+
+    // Listen on chat-message
+    socket.on('chat-message', (data) => {
+        io.sockets.emit('chat-message', data);
+    });
+
+    // Listen on typing
+    socket.on('typing', (data) => {
+        io.sockets.emit('typing');
+    });
+});
+
+
+http.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000')
+});
